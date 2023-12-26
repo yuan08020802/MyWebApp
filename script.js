4107e5a17c1a43febd2186060d33f64d718a19ce
yuan08020802 <154298422+yuan08020802@users.noreply.github.com>
2023-12-26 11:50:40 +0800

    7

--- script.js
+++ script.js
@@ -1,30 +1,130 @@
-const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
-const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const playerDisplay = document.querySelector('#player')
+    const result = document.querySelector('#result')
+    const restart = document.querySelector('#restart')
+    const squareArray = Array.from(squares)
+    const winList = Array.of([0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6])
 
-const keys = document.querySelectorAll('.key')
-const whiteKeys = document.querySelectorAll('.key.white')
-const blackKeys = document.querySelectorAll('.key.black')
+    let currentPlayer = 'playerX'
+    let end = ''
+    let Xmark = []
+    let Omark = []
 
-keys.forEach(key => {
-  key.addEventListener('click', () => playNote(key))
+    squares.forEach(square => {
+        square.addEventListener('click', startGame)
+    })
+
+    restart.addEventListener('click', restartGame)
+
+    function startGame(e) {
+        const index = squareArray.indexOf(e.target)
+        const choose = squareArray[index]
+
+        if (choose.classList.contains('playerO') || choose.classList.contains('playerX')) {
+            return
+        }
+
+        if (currentPlayer === 'playerX') {
+            if (choose.classList.contains('playerO')) {
+                return
+            } else {
+                currentPlayer = 'playerO'
+                choose.classList.add('playerX')
+            }
+        } else if (currentPlayer === 'playerO') {
+            if (choose.classList.contains('playerX')) {
+                return
+            } else {
+                currentPlayer = 'playerX'
+                choose.classList.add('playerO')
+            }
+        }
+
+        playerDisplay.innerHTML = currentPlayer
+
+        addMark(index)
+        checkWin()
+    }
+
+    function restartGame() {
+        squares.forEach(square => {
+            if (square.classList.contains('playerO')) {
+                square.classList.remove('playerO')
+            }
+            if (square.classList.contains('playerX')) {
+                square.classList.remove('playerX')
+            }
+        })
+        Xmark = []
+        Omark = []
+        currentPlayer = 'playerX'
+        playerDisplay.innerHTML = currentPlayer
+        end = '結果'
+        result.innerHTML = end
+        if (result.getAttribute('endState') === 'true') {
+            squares.forEach(square => {
+                square.addEventListener('click', startGame)
+            })
+        }
+    }
+
+    function addMark(index) {
+        if (currentPlayer === 'playerX') {
+            Xmark.push(index)
+            Xmark.sort((a, b) => {
+                return a - b
+            })
+        }
+        if (currentPlayer === 'playerO') {
+            Omark.push(index)
+            Omark.sort((a, b) => {
+                return a - b
+            })
+        }
+    }
+
+    function checkWin() {
+        if (XWin()) {
+            end = 'playerX Win'
+            result.innerHTML = end
+            result.setAttribute('endState', 'true')
+            squareArray.forEach(square => square.removeEventListener('click', startGame))
+            alert('playerX Win')
+        } else if (YWin()) {
+            end = 'playerO Win'
+            result.innerHTML = end
+            result.setAttribute('endState', 'true')
+            squareArray.forEach(square => square.removeEventListener('click', startGame))
+            alert('playerO Win')
+        } else if (isDraw()) {
+            end = 'draw'
+            result.innerHTML = end
+            result.setAttribute('endState', 'true')
+            alert('draw')
+        }
+    }
+
+    function XWin() {
+        return winList.some(array => {
+            return array.every(index => {
+                return squareArray[index].classList.contains('playerX')
+            })
+        })
+    }
+
+    function YWin() {
+        return winList.some(array => {
+            return array.every(index => {
+                return squareArray[index].classList.contains('playerO')
+            })
+        })
+    }
+
+    function isDraw() {
+        return squareArray.every(square => {
+            return square.classList.contains('playerX') || square.classList.contains('playerO')
+        })
+    }
+
 })
-
-document.addEventListener('keydown', e => {
-  if (e.repeat) return
-  const key = e.key
-  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
-  const blackKeyIndex = BLACK_KEYS.indexOf(key)
-
-  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
-  if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
-})
-
-function playNote(key) {
-  const noteAudio = document.getElementById(key.dataset.note)
-  noteAudio.currentTime = 0
-  noteAudio.play()
-  key.classList.add('active')
-  noteAudio.addEventListener('ended', () => {
-    key.classList.remove('active')
-  })
-}
